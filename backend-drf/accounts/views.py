e67d234b9acf1ee5611a3bad0c8fe2edf32df2c6
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2026-07-04 00:28:21 +0600

    add user registaion api

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListCreateAPIView
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserListCreateApiView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return User.objects.all()
