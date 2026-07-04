00c6f02abc6b7c4dd919baed83204e50f9d93288
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2026-07-04 19:55:38 +0600

    add registaion page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import './assets/css/style.css';
-import Headers from './components/Header'
 import Main from './components/Main'
+import Register from './components/Register'
+import Headers from './components/Header'
 import Footer from './components/Footer'
-import Button from './components/Button';
+import Login from './components/Login'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <Headers />
-      <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       <Footer />
-      <Button />
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend-react/src/components/Register.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSpinner} from '@fortawesome/free-solid-svg-icons'
+
+const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState({})
+    const [success, setSuccess] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const handleRegistration = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const userData = {
+            username: username,
+            email: email,
+            password: password
+        }
+        // Here you would typically make an API call to register the user
+        // axios.post('http://127.0.0.1:8000/api/v1/register/', userData)
+        try {
+            const response = await axios.post(
+            "http://127.0.0.1:8000/api/v1/register/",
+            userData
+        );
+
+        console.log("User registered successfully:", response.data);
+
+        setError({});
+        setSuccess(true);
+
+    } catch (error) {
+        console.log(error.response);
+
+        if (error.response) {
+            setError(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+    }finally {
+        setLoading(false);
+    }
+  }
+
+  return (
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 bg-light-dark p-5 mx-auto mt-5 rounded">
+                    <h3 className="text-light text-center mb-4">Create Account</h3>
+                    <form onSubmit={handleRegistration}>
+                        <div className='mb-3'>
+                            <input type="text" className="form-control mb-3" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <small>{error.username && <div className="text-danger">{error.username}</div>}</small>
+                        </div>
+                        <div className='mb-3'>
+                            <input type="email" className="form-control mb-3" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <small>{error.email && <div className="text-danger">{error.email}</div>}</small>
+                        </div>
+                        <div className='mb-3'>
+                            <input type="password" className="form-control mb-3" placeholder="Set password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            <small>{error.password && <div className="text-danger">{error.password}</div>}</small>
+                        </div>
+                        {success && <div className="alert alert-success">User registered successfully!</div>}
+                        <button type="submit" className="btn btn-info d-block mx-auto" disabled ={loading}>
+                            {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : 'Register'}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Register
